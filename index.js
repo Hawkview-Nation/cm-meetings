71e56f08901c11bf05f5dd7500e03d8cec9fb01d
Hawkview-Nation <bstick2008@yahoo.com>
2026-07-18 04:37:54 -0400

    Set up Express server to serve static files

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+// Serve static files (like index.html)
+app.use(express.static(__dirname));
+
+// Start the server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
 
