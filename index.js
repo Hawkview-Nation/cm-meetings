d55c11a30e4f0841146673804cba59476c185e1e
Hawkview-Nation <bstick2008@yahoo.com>
2026-07-29 05:21:59 -0400

    Add route to serve index.html file

--- index.js
+++ index.js
@@ -9,6 +9,10 @@ const app = express();
 
 app.use(express.static(__dirname));
 
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
